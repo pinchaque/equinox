d78c51f414112eb1010565fc19539d639a5b3cf9
Chuck Smith <chuck@pinchaque.com>
2024-02-13 16:24:10 -0600

    finished up initial implementation of datafile and basic unit tests

--- internal/datafile_test.go
+++ internal/datafile_test.go
@@ -18,7 +18,7 @@ func tempFileName() (string, error) {
 	return f.Name(), nil
 }
 
-func getPoint(i int) *Point {
+func getPoint(i uint32) *Point {
 	ts := time.Date(2024, 01, 10, 23, 1, 2, 3, time.UTC)
 	dur, err := time.ParseDuration(fmt.Sprintf("%dm", i))
 	if err != nil {
@@ -32,32 +32,137 @@ func getPoint(i int) *Point {
 	return p
 }
 
-func TestDFWriteRead(t *testing.T) {
+func TestDFNew(t *testing.T) {
 	fn, err := tempFileName()
 	if err != nil {
 		t.Fatalf("failed to get temp file name: %s", err.Error())
 	}
-	//defer os.Remove(fn)
-	t.Logf("temp file: %s", fn)
+	defer os.Remove(fn)
 
-	df := OpenFile(fn)
-	defer df.CloseFile()
+	ser := NewSerializer()
 
-	for i := 0; i < 10; i++ {
+	b, _ := ser.Serialize(getPoint(0))
+
+	df, err := OpenNewDF(fn, ser, uint32(len(b)))
+	if err != nil {
+		t.Fatalf("failed to open file %s with recsize %d: %s", fn, len(b), err.Error())
+	}
+	defer df.Close()
+
+	var i uint32
+	for i = 0; i < 10; i++ {
 		p := getPoint(i)
-		df.Write(i, p)
+		err := df.Write(i, p)
+		if err != nil {
+			t.Errorf("error while reading point %d: %s", i, err.Error())
+		}
 	}
 
-	for i := 0; i < 1; i++ {
+	// read back from the same DataFile
+	for i = 0; i < 10; i++ {
 		p := getPoint(i)
 		p2, err := df.Read(i)
 
 		if err != nil {
-			t.Errorf("error while reading poing %d: %s", i, err.Error())
+			t.Errorf("error while reading point %d: %s", i, err.Error())
+			continue
 		}
 
 		if !p.Equal(p2) {
 			t.Errorf("expected %s, got %s", p.String(), p2.String())
+		}
+	}
+
+	// read back from a new DataFile object
+	df2, err := OpenExistingDF(fn, ser)
+	if err != nil {
+		t.Fatalf("failed to open existing file %s: %s", fn, err.Error())
+	}
+
+	for i = 9; i >= 1; i-- {
+		p := getPoint(i)
+		p2, err := df2.Read(i)
+
+		if err != nil {
+			t.Errorf("error while reading point %d: %s", i, err.Error())
+			continue
+		}
+
+		if !p.Equal(p2) {
+			t.Errorf("expected %s, got %s", p.String(), p2.String())
+		}
+	}
+
+}
+
+func TestDFMissing(t *testing.T) {
+	fn, err := tempFileName()
+	if err != nil {
+		t.Fatalf("failed to get temp file name: %s", err.Error())
+	}
+	defer os.Remove(fn)
+
+	ser := NewSerializer()
+
+	_, err = OpenExistingDF(fn, ser)
+	if err == nil {
+		t.Fatalf("expected to fail to open non-existent file %s", fn)
+	}
+}
+
+// tests writing/reading non-sequential points
+func TestDFNonseq(t *testing.T) {
+	fn, err := tempFileName()
+	if err != nil {
+		t.Fatalf("failed to get temp file name: %s", err.Error())
+	}
+	defer os.Remove(fn)
+
+	ser := NewSerializer()
+
+	b, _ := ser.Serialize(getPoint(0))
+
+	df, err := OpenNewDF(fn, ser, uint32(len(b)))
+	if err != nil {
+		t.Fatalf("failed to open file %s with recsize %d: %s", fn, len(b), err.Error())
+	}
+	defer df.Close()
+
+	// write points 0, 2
+	df.Write(0, getPoint(0))
+	df.Write(2, getPoint(2))
+
+	// point 0 and 2 should work
+	for i := 0; i < 3; i += 2 {
+		p := getPoint(uint32(i))
+		p2, err := df.Read(uint32(i))
+
+		if err != nil {
+			t.Errorf("error while reading point %d: %s", i, err.Error())
+		}
+
+		if !p.Equal(p2) {
+			t.Errorf("expected %s, got %s", p.String(), p2.String())
+		}
+	}
+
+	// point 3+ should fail
+	for i := 3; i < 600; i += 100 {
+		p2, err := df.Read(uint32(i))
+		if err == nil {
+			t.Errorf("should have failed to read point %d; got %s", i, p2.String())
+		} else {
+			//t.Logf("correctly got error when reading point %d: %s", i, err.Error())
+		}
+	}
+
+	// point 1 should fail
+	for i := 1; i < 2; i++ {
+		p2, err := df.Read(uint32(i))
+		if err == nil {
+			t.Errorf("should have failed to read point %d; got %s", i, p2.String())
+		} else {
+			//t.Logf("correctly got error when reading point %d: %s", i, err.Error())
 		}
 	}
 }
