28f559f706e4f7f23e89d8b2fca204b005504c81
Chuck Smith <chuck@pinchaque.com>
2024-02-21 18:20:56 -0600

    simplify test with closure

--- internal/memlist_test.go
+++ internal/memlist_test.go
@@ -9,55 +9,22 @@ func TestMemListConstructBasic(t *testing.T) {
 	ps := getPoints(0, 10)
 	var err error
 
-	ml.Add(ps[3:5])
-	err = ml.validate()
-	if err != nil {
-		t.Fatalf("validation failed: %s", err.Error())
-	}
-	//t.Logf("after adding 3:4: %s", ml.String())
-	if ml.Len() != 2 {
-		t.Fatalf("incorrect length expected 2 got %d", ml.Len())
+	runtest := func(p []*Point, len int) {
+		ml.Add(p)
+		err = ml.validate()
+		if err != nil {
+			t.Fatalf("validation failed: %s", err.Error())
+		}
+		if ml.Len() != len {
+			t.Fatalf("incorrect length expected %d got %d", len, ml.Len())
+		}
 	}
 
-	ml.Add(ps[0:2])
-	err = ml.validate()
-	if err != nil {
-		t.Fatalf("validation failed: %s", err.Error())
-	}
-	if ml.Len() != 4 {
-		t.Fatalf("incorrect length expected 4 got %d", ml.Len())
-	}
-	//t.Logf("after adding 0:1: %s", ml.String())
-
-	ml.Add(ps[2:3])
-	err = ml.validate()
-	if err != nil {
-		t.Fatalf("Validation failed: %s", err.Error())
-	}
-	if ml.Len() != 5 {
-		t.Fatalf("incorrect length expected 5 got %d", ml.Len())
-	}
-	//t.Logf("after adding 2: %s", ml.String())
-
-	ml.Add(ps[5:7])
-	err = ml.validate()
-	if err != nil {
-		t.Fatalf("Validation failed: %s", err.Error())
-	}
-	if ml.Len() != 7 {
-		t.Fatalf("incorrect length expected 7 got %d", ml.Len())
-	}
-	//t.Logf("after adding 5:6: %s", ml.String())
-
-	ml.Add([]*Point{ps[9], ps[8], ps[7]})
-	err = ml.validate()
-	if err != nil {
-		t.Fatalf("Validation failed: %s", err.Error())
-	}
-	if ml.Len() != 10 {
-		t.Fatalf("incorrect length expected 10 got %d", ml.Len())
-	}
-	//t.Logf("after adding 9:7: %s", ml.String())
+	runtest(ps[3:5], 2)
+	runtest(ps[0:2], 4)
+	runtest(ps[2:3], 5)
+	runtest(ps[5:7], 7)
+	runtest([]*Point{ps[9], ps[8], ps[7]}, 10)
 }
 
 func TestMemListConstructBatches(t *testing.T) {
