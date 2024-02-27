af23fc1678c575564d84c2fb9855b9f0bc4aa5ff
Chuck Smith <chuck@pinchaque.com>
2024-02-27 15:59:10 -0600

    enable remaining tests for memtree

--- internal/memtree_test.go
+++ internal/memtree_test.go
@@ -6,13 +6,11 @@ import (
 
 func TestMemTreeQuery(t *testing.T) {
 	testPointIO(t, NewMemTree(), 10, 5)
-	/*
-		testPointIO(t, NewMemTree(), 10, 10)
-		testPointIO(t, NewMemTree(), 10, 4)
-		testPointIO(t, NewMemTree(), 10, 1)
-		testPointIO(t, NewMemTree(), 100, 9)
-		testPointIO(t, NewMemTree(), 100, 10)
-		testPointIO(t, NewMemTree(), 1000, 49)
-		testPointIO(t, NewMemTree(), 1000, 50)
-	*/
+	testPointIO(t, NewMemTree(), 10, 10)
+	testPointIO(t, NewMemTree(), 10, 4)
+	testPointIO(t, NewMemTree(), 10, 1)
+	testPointIO(t, NewMemTree(), 100, 9)
+	testPointIO(t, NewMemTree(), 100, 10)
+	testPointIO(t, NewMemTree(), 1000, 49)
+	testPointIO(t, NewMemTree(), 1000, 50)
 }
