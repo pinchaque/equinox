b95277d6d13d8f07e6845936eddbe0debf95e3ed
Chuck Smith <chuck@pinchaque.com>
2024-02-28 11:18:53 -0600

    Add exists and logical and/or/not

--- internal/queryattr.go
+++ internal/queryattr.go
@@ -3,6 +3,7 @@ package equinox
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 // This is the generic interface for querying against attributes that is used
@@ -15,9 +16,29 @@ type QueryAttr interface {
 	String() string
 }
 
-/*
-	TODO: not and or
-*/
+/****************************************************************************
+	QAExist - Exists
+****************************************************************************/
+
+// Represents whether an attribute exists
+type QAExists struct {
+	k string
+}
+
+// Returns true if this query attribute exists (can be empty)
+func (qa *QAExists) Match(attrs map[string]string) bool {
+	_, exists := attrs[qa.k]
+	return exists
+}
+
+func (qa *QAExists) String() string {
+	return fmt.Sprintf("%s exists", qa.k)
+}
+
+// Returns new QAExists object with specified attribute key
+func NewQAExists(k string) *QAExists {
+	return &QAExists{k: k}
+}
 
 /****************************************************************************
 	QAEqual - Equality
@@ -77,4 +98,108 @@ func (qa *QARegex) String() string {
 func NewQARegex(k string, regex string) *QARegex {
 	re := regexp.MustCompile(regex)
 	return &QARegex{k: k, re: re}
+}
+
+/****************************************************************************
+	QANot - Logical NOT
+****************************************************************************/
+
+// Represents logical inversion (NOT)
+type QANot struct {
+	qa QueryAttr
+}
+
+// Returns logical inversion (NOT) of the contained QueryAttr
+func (qa *QANot) Match(attrs map[string]string) bool {
+	return !qa.qa.Match(attrs)
+}
+
+func (qa *QANot) String() string {
+	return fmt.Sprintf("!(%s)", qa.qa.String())
+}
+
+// Returns new QANot object that's the logical inversion of the specified
+// QueryAttr
+func NewQANot(qa QueryAttr) *QANot {
+	return &QANot{qa: qa}
+}
+
+/****************************************************************************
+	QAAnd - Logical AND
+****************************************************************************/
+
+// Represents logical conjunction (AND)
+type QAAnd struct {
+	qa []QueryAttr
+}
+
+// Returns logical conjunction (AND) of the contained QueryAttrs
+func (qa *QAAnd) Match(attrs map[string]string) bool {
+	if len(qa.qa) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(qa.qa); i++ {
+		if !qa.qa[i].Match(attrs) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (qa *QAAnd) String() string {
+	var ret []string
+
+	for i := 0; i < len(qa.qa); i++ {
+		ret = append(ret, "("+qa.qa[i].String()+")")
+	}
+
+	return strings.Join(ret, " && ")
+}
+
+// Returns new QAAnd object that's the logical inversion of the specified
+// QueryAttr
+func NewQAAnd(qa ...QueryAttr) *QAAnd {
+	return &QAAnd{qa: qa}
+}
+
+/****************************************************************************
+	QAOr - Logical OR
+****************************************************************************/
+
+// Represents logical disjunction (OR)
+type QAOr struct {
+	qa []QueryAttr
+}
+
+// Returns logical disjunction (OR) of the contained QueryAttrs
+func (qa *QAOr) Match(attrs map[string]string) bool {
+	if len(qa.qa) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(qa.qa); i++ {
+		if qa.qa[i].Match(attrs) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (qa *QAOr) String() string {
+	var ret []string
+
+	for i := 0; i < len(qa.qa); i++ {
+		ret = append(ret, "("+qa.qa[i].String()+")")
+	}
+
+	return strings.Join(ret, " || ")
+}
+
+// Returns new QAOr object that's the logical inversion of the specified
+// QueryAttr
+func NewQAOr(qa ...QueryAttr) *QAOr {
+	return &QAOr{qa: qa}
 }
