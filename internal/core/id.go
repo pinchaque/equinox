9c95f77bff82827b2e55c5b94e999b7dc600f3e0
Chuck Smith <chuck@pinchaque.com>
2024-03-03 21:30:29 -0700

    Add new class for representing IDs

---
+++ internal/core/id.go
@@ -0,0 +1,71 @@
+package core
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+)
+
+// Representation of an ID used for data points
+type Id struct {
+	val uint64
+}
+
+// Creates a new random ID
+func NewId() *Id {
+	id := Id{val: rand.Uint64()}
+	return &id
+}
+
+// Creates an Id struct from the specified string, which must be a Base64 Url
+// Encoding representing a uint64.
+func IdFromString(s string) (*Id, error) {
+	b, err := base64.URLEncoding.DecodeString(s)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding string '%s': %s", s, err.Error())
+	}
+
+	// check byte length
+	if len(b) != 8 {
+		return nil, fmt.Errorf("invalid num bytes %d when decoding string '%s'", len(b), s)
+	}
+
+	// read into a uint64
+	var i uint64
+	buf := bytes.NewReader(b)
+	err = binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return nil, fmt.Errorf("error reading uint64 from string '%s': %s", s, err.Error())
+	}
+
+	// create Id struct and return
+	id := Id{val: i}
+	return &id, nil
+}
+
+// Returns string representation of an ID, which is a Base64 Url Encoding
+func (id *Id) String() string {
+	var buf bytes.Buffer
+	err := binary.Write(&buf, binary.BigEndian, id.val)
+	if err != nil {
+		panic(fmt.Sprintf("failed to write id %d to string", id.val))
+	}
+
+	return base64.URLEncoding.EncodeToString(buf.Bytes())
+}
+
+// Compares two Id structs, return -1 if this one is less than other, 1 if this
+// is greater than other, 0 if equal. This can be used to check for uniqueness
+// and duplicate IDs. The current implementation just compares the underlying
+// uint64 values.
+func (id *Id) Cmp(oth *Id) int {
+	if id.val < oth.val {
+		return -1
+	} else if id.val > oth.val {
+		return 1
+	} else {
+		return 0
+	}
+}
---
+++ internal/core/id_test.go
@@ -0,0 +1,103 @@
+package core
+
+import (
+	"testing"
+)
+
+func TestIdString(t *testing.T) {
+	id := Id{val: 2822340188419286878}
+	exp := "Jyr3cq4KZ14="
+
+	if id.String() != exp {
+		t.Errorf("error in id string: expeced %s got %s", exp, id.String())
+	}
+}
+
+func TestIdRoundtrip(t *testing.T) {
+	// run this a bunch of times
+	for i := 0; i < 500; i++ {
+
+		id1 := NewId()
+		s := id1.String()
+		id2, err := IdFromString(s)
+		if err != nil {
+			t.Errorf("failed to create Id from string %s: %s", s, err.Error())
+		}
+
+		if id1.val != id2.val {
+			t.Errorf("Id roundtrip values not equal, expected %d got %d", id1.val, id2.val)
+		}
+
+		if id1.String() != id2.String() {
+			t.Errorf("Id roundtrip strings not equal, expected %s got %s", id1.String(), id2.String())
+		}
+	}
+}
+
+func TestIdErrors(t *testing.T) {
+	var id *Id
+	var err error
+	var s, msg string
+
+	// missing the trailing "=" that is padding
+	s = "Jyr3cq4KZ14"
+	msg = "error decoding string 'Jyr3cq4KZ14': illegal base64 data at input byte 8"
+	id, err = IdFromString(s)
+	if err == nil {
+		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
+	} else if err.Error() != msg {
+		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
+			s, msg, err.Error())
+	}
+
+	// garbage
+	s = "Jyr$*#()"
+	msg = "error decoding string 'Jyr$*#()': illegal base64 data at input byte 3"
+	id, err = IdFromString(s)
+	if err == nil {
+		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
+	} else if err.Error() != msg {
+		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
+			s, msg, err.Error())
+	}
+
+	// empty string
+	s = ""
+	msg = "invalid num bytes 0 when decoding string ''"
+	id, err = IdFromString(s)
+	if err == nil {
+		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
+	} else if err.Error() != msg {
+		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
+			s, msg, err.Error())
+	}
+
+	// partial string
+	s = "Jyr3cq4K"
+	msg = "invalid num bytes 6 when decoding string 'Jyr3cq4K'"
+	id, err = IdFromString(s)
+	if err == nil {
+		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
+	} else if err.Error() != msg {
+		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
+			s, msg, err.Error())
+	}
+}
+
+func TestIdCmp(t *testing.T) {
+	id1 := &Id{val: 12345678}
+	id2 := &Id{val: 45678912}
+
+	fn := func(i1 *Id, i2 *Id, exp int) {
+		act := i1.Cmp(i2)
+		if act != exp {
+			t.Errorf("error comparing [%d]%s to [%d]%s: expected %d got %d",
+				i1.val, i1.String(), i2.val, i2.String(), exp, act)
+		}
+	}
+
+	fn(id1, id2, -1)
+	fn(id2, id1, 1)
+	fn(id1, id1, 0)
+	fn(id2, id2, 0)
+}
