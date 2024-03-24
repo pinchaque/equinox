e2071628e3e300e97b925e80f91dc74d0813fa41
Chuck Smith <chuck@pinchaque.com>
2024-03-24 12:37:24 -0700

    Convert to using Testify

--- internal/core/id_test.go
+++ internal/core/id_test.go
@@ -40,68 +40,43 @@ func TestIdRoundtrip(t *testing.T) {
 		id1 := NewId()
 		s := id1.String()
 		id2, err := IdFromString(s)
-		if err != nil {
-			t.Errorf("failed to create Id from string %s: %s", s, err.Error())
-		}
-
-		if id1.val != id2.val {
-			t.Errorf("Id roundtrip values not equal, expected %d got %d", id1.val, id2.val)
-		}
-
-		if id1.String() != id2.String() {
-			t.Errorf("Id roundtrip strings not equal, expected %s got %s", id1.String(), id2.String())
-		}
+		assert.Nil(t, err)
+		assert.Equal(t, id1.val, id2.val)
+		assert.Equal(t, id1.String(), id2.String())
 	}
 }
 
 func TestIdErrors(t *testing.T) {
-	var id *Id
 	var err error
 	var s, msg string
 
 	// missing the trailing "=" that is padding
 	s = "Jyr3cq4KZ14"
 	msg = "error decoding string 'Jyr3cq4KZ14': illegal base64 data at input byte 8"
-	id, err = IdFromString(s)
-	if err == nil {
-		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
-	} else if err.Error() != msg {
-		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
-			s, msg, err.Error())
-	}
+	_, err = IdFromString(s)
+	assert.NotNil(t, err)
+	assert.Equal(t, msg, err.Error())
 
 	// garbage
 	s = "Jyr$*#()"
 	msg = "error decoding string 'Jyr$*#()': illegal base64 data at input byte 3"
-	id, err = IdFromString(s)
-	if err == nil {
-		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
-	} else if err.Error() != msg {
-		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
-			s, msg, err.Error())
-	}
+	_, err = IdFromString(s)
+	assert.NotNil(t, err)
+	assert.Equal(t, msg, err.Error())
 
 	// empty string
 	s = ""
 	msg = "invalid num bytes 0 when decoding string ''"
-	id, err = IdFromString(s)
-	if err == nil {
-		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
-	} else if err.Error() != msg {
-		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
-			s, msg, err.Error())
-	}
+	_, err = IdFromString(s)
+	assert.NotNil(t, err)
+	assert.Equal(t, msg, err.Error())
 
 	// partial string
 	s = "Jyr3cq4K"
 	msg = "invalid num bytes 6 when decoding string 'Jyr3cq4K'"
-	id, err = IdFromString(s)
-	if err == nil {
-		t.Errorf("expected error when parsing %s, got success (%d, %s)", s, id.val, id.String())
-	} else if err.Error() != msg {
-		t.Errorf("unexpected error message when parsing %s: expected '%s' got '%s'",
-			s, msg, err.Error())
-	}
+	_, err = IdFromString(s)
+	assert.NotNil(t, err)
+	assert.Equal(t, msg, err.Error())
 }
 
 func TestIdCmp(t *testing.T) {
@@ -110,10 +85,7 @@ func TestIdCmp(t *testing.T) {
 
 	fn := func(i1 *Id, i2 *Id, exp int) {
 		act := i1.Cmp(i2)
-		if act != exp {
-			t.Errorf("error comparing [%d]%s to [%d]%s: expected %d got %d",
-				i1.val, i1.String(), i2.val, i2.String(), exp, act)
-		}
+		assert.Equal(t, exp, act)
 	}
 
 	fn(id1, id2, -1)
